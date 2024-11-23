3a9f503a9484c36fd02e622492001a993abf98f3
Azarielh <33382899+Azarielh@users.noreply.github.com>
2024-11-23 09:20:46 +0100

    Update ft_strnstr.c

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -12,36 +12,25 @@
 
 #include "libft.h"
 
-static size_t	is_partof_big(const char c_of_b, const char c_of_l)
-{
-	return (c_of_b == c_of_l);
-}
-
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	size_t	start;
-	int		is_little_len;
-
-	if (!little)
-		return ((char *)big);
+	int	finding;
+	
+	if (!to_find)
+		return ((char *)str);
 	i = 0;
 	j = 0;
-	start = 0;
-	is_little_len = (i == (size_t)ft_strlen(little));
-	while (j < len && big[start])
+	finding = (str[i + j] == to_find[j]);
+	while (i < len && str[i])
 	{
-		i = 0;
-		while (is_partof_big(big[start], little[i]))
-		{
-			start++;
-			i++;
-		}
-		if (is_little_len)
-			return ((char *)big + (start - i));
-		start++;
-		j++;
+		j = 0;
+		while (finding && i < len)
+			j++;
+		if (to_find[j] == '\0')
+			return (&((char *)str)[i]);
+		i++;
 	}
 	return (NULL);
 }
