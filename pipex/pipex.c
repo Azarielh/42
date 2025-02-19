bba333e0ff770866dd28c1b2fe7aa5042d893680
{azarielh42} <{madstudent@42.skynet}>
2025-02-19 23:18:26 +0100

    Init clean headers with params

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: jlacaze- <jlacaze-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 15:05:53 by jlacaze-          #+#    #+#             */
-/*   Updated: 2025/02/17 18:51:18 by jlacaze-         ###   ########.fr       */
+/*   Updated: 2025/02/19 22:40:24 by jlacaze-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,13 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <string.h>
 #include "libft/libft.h"
+#include <fcntl.h>
 
+// _______________________ GETTING TO TERMINAL CMD __________________________________________
 
-char *parsing_cmd_from_env(char **argv, const void *cmd)
-{
-    //argv is the string passed as argument when program is called upon
-    //cmd is dest for cmd storage with their respective paramaters
-    int i;
-    int cmd_len;
-    
-    i = 0;
-    cmd_len = 0;
-    while (argv[i][cmd_len++])
-    {
-        if(argv[i][cmd_len + 1] == ' ')
-            ft_memmove(cmd, argv[i], cmd_len);
-        if(argv[i][cmd_len + 1] == '-')
-            ft_memmove(cmd, argv[i], cmd_len);
-    }
-}
 char *get_path_from_env(char **env)		// Find Path environment
 {
 	while (*env)
@@ -62,21 +48,64 @@ char *find_command_path(char *cmd, char **env)
     return NULL;
 }
 
+// _____________________________ EXECUTE THEM ________________________________
+void execute_command(char *cmd, char *file_src, char *file_dest, char **env) 
+{
+    // char *cmd_path = find_command_path(cmd, env);
+    // if (!cmd_path) {
+    //     ft_printf("\033[1;31mCommand not found: %s\033[0m\n", cmd);
+    //     exit(EXIT_FAILURE);
+    // }
 
-void execute_command(char *cmd, char *file, char **env) {
-    char *cmd_path = find_command_path(cmd, env);
-    if (!cmd_path) {
-        ft_printf("\033[1;31mCommand not found: %s\033[0m\n", cmd);
+    // Créer un tableau args propre
+
+    // char *args[] = {cmd, file_src, NULL};  // On passe la commande et le fichier d'entrée
+
+    // ft_printf("\n\033[1;36mExecuting command:\033[0m %s\n\033[1;32mFile content :\n\033[1;0m", cmd_path);
+
+    // Gerer l'ouverture du fichier
+
+    int fd =   fd = open(file_dest, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+    if (fd == -1) 
+    {
+        perror("Erreur lors de la création du fichier");
         exit(EXIT_FAILURE);
     }
 
-    // Créer un tableau args propre
-    char *args[] = {cmd, file, NULL};  // On passe la commande et le fichier d'entrée
+    pid_t child = fork();
 
-    ft_printf("\n\033[1;36mExecuting command:\033[0m %s\n\033[1;32mFile content :\n\033[1;0m", cmd_path);
-    execve(cmd_path, args, env);
-    perror("execve failed");
-    exit(EXIT_FAILURE);
+    if (child < 0)
+    {
+        // Error while creating process
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    } else if (child == 0) {
+        // This is executed by the child's process
+        ft_printf("Je suis le child %d\n", getpid());
+        // Execute of new program within the child process
+            char *cmd_path = find_command_path(cmd, env);
+        ft_printf("\n\033[1;36mExecuting command:\033[0m %s\n\033[1;32mFile content :\n\033[1;0m", cmd_path);
+
+        if (!cmd_path) {
+            ft_printf("\033[1;31mCommand not found: %s\033[0m\n", cmd);
+            exit(EXIT_FAILURE);
+        }
+
+        char *args[] = {cmd, file_src, NULL};  // On passe la commande et le fichier d'entrée
+        execve(cmd_path, args, env);
+        // Si execve failed
+        perror("execve failed");
+        exit (EXIT_FAILURE);
+    } else {
+        // Bloc exécuté par le processus parent
+        ft_printf("\nJe suis le processus parent avec PID %d\n", getpid());
+        // Attente de la fin du processus enfant
+        int status;
+        pid_t child_pid
+        waitpid(child, );
+        ft_printf("\nLe processus enfant est terminé.\n");
+    }
 }
 
 
@@ -87,10 +116,11 @@ int main(int argc, char **argv, char **env) {
         return (EXIT_FAILURE);
     }
 
-    char *file1 = argv[1];  // File or input to work with
+    char *file_src = argv[1];  // File or input to work with
     char *cmd = argv[2];   // cmd to execute
+    char *file_dest = argv[argc - 1];
 
-    execute_command(cmd, file1, env);  // Passer correctement les arguments
+    execute_command(cmd, file_src, file_dest, env);  // Passer les arguments
 
     return (0);
 }
